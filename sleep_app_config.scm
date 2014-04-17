46e3c471227d41e0dcd9665d7b675320206dcb88
Amos Folarin <amosfolarin@gmail.com>
2014-04-17 11:48:54 +0100

    scm file seemed to be missing bits -- fixed?

--- sleep_app_config.scm
+++ sleep_app_config.scm
@@ -1,3 +1,3 @@
-(pr-update-probe '(("name" . "edu.northwestern.cbits.purple_robot_manager.probes.builtin.LocationProbe") 
+(begin (pr-update-probe '(("name" . "edu.northwestern.cbits.purple_robot_manager.probes.builtin.LocationProbe") 
 ("frequency" . 300000L) 
-("enabled" . #t)
+("enabled" . #t))))
